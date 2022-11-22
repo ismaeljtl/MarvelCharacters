2e254e1e5caf91be1d77f22c5bad4e8949754d46
Ismael Teixeira <ismael@MBP-de-Ismael.home>
2022-11-22 22:58:32 +0000

    Obteniendo todos los resultados al mismo tiempo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,18 +4,18 @@ import { CharactersService } from 'src/app/services/characters.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   loading: boolean = false;
   characters: any = undefined;
   filteredCharacters: any = undefined;
   totalCharacters: number = 0;
   p: number = 1;
   offset: number = 0;
+  limit: number = 30;
 
-  constructor(private characterService: CharactersService) { }
+  constructor(private characterService: CharactersService) {}
 
   ngOnInit(): void {
     this.getCharacters();
@@ -24,10 +24,21 @@ export class HomeComponent implements OnInit {
   async getCharacters() {
     this.loading = true;
     try {
-      const res = await this.characterService.getCharacters(this.offset);
-      this.characters = res.data.results
-      this.filteredCharacters = this.characters;
+      const res = await this.characterService.getCharacters(
+        this.offset,
+        this.limit
+      );
       this.totalCharacters = res.data.total;
+      const limit = Math.ceil(this.totalCharacters / this.limit);
+      let fetchArr: Array<unknown> = [];
+      for (let i = 0; i < limit; i++) {
+        fetchArr.push(this.characterService.getCharacters(limit * i, limit));
+      }
+      const data = await Promise.all(fetchArr);
+      this.characters = data.map((item: any) => item.data.results).flat();
+
+      this.filteredCharacters = this.characters;
+      this.totalCharacters = this.characters.length;
     } catch (error) {
       alert('An error has occurred, please try again.');
       console.log(error);
@@ -39,21 +50,21 @@ export class HomeComponent implements OnInit {
     this.p = page;
     // we have to substract 1 to page because the first page have an offset of 0
     this.offset = 30 * (page - 1);
-    this.getCharacters();
   }
 
   filter(e: any) {
     const value = e.target.value;
-    
+
     if (this.characters.length !== 0) {
       if (value === '') {
         this.filteredCharacters = this.characters;
       } else {
-        this.filteredCharacters = this.characters.filter((character: { name: string; }) => {
-          return character.name.toLowerCase().includes(value.toLowerCase()) 
-        }).sort();
+        this.filteredCharacters = this.characters
+          .filter((character: { name: string }) => {
+            return character.name.toLowerCase().includes(value.toLowerCase());
+          })
+          .sort();
       }
     }
   }
-
 }
--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,30 +1,25 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import {Md5} from 'ts-md5/dist/md5';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharactersService {
-
   url = environment.apiUrl;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  getCharacters(offset: number, limit: number): any {
+    return this.http
+      .get(this.url + `/v1/public/characters`, {
+        params: new HttpParams().set('limit', limit).set('offset', offset),
+      })
+      .toPromise();
   }
 
-  getCharacters(offset: number): any{
-    return this.http.get(
-      this.url + `/v1/public/characters`, {
-        params: new HttpParams().set('limit', 30).set('offset', offset)
-      }
-    ).toPromise();
+  getCharacter(id: number): any {
+    return this.http.get(this.url + `/v1/public/characters/${id}`).toPromise();
   }
-
-  getCharacter(id: number): any{
-    return this.http.get(
-      this.url + `/v1/public/characters/${id}`
-    ).toPromise();
-  }
-
 }
